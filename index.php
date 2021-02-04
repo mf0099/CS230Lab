746bde75160551fabb0dabb00435bfb512d4f1e9
mf0099 <mf0099@mix.wvu.edu>
2021-02-03 23:39:19 -0500

    lab 2 (about page)

---
+++ index.php
@@ -0,0 +1,54 @@
+<body>
+    <link rel="stylesheet" href ="css/about.css">
+
+    <div class="about">
+        <h1>About Me</h1>
+        <h2>Morgan Fraley </h2>
+        <p>CEO of Meme-ories</p>
+        <p>Here you will find a vast collection and reviews of memes from over the years.
+            From the old classics that will never grow old to the new trending memes,
+            you can view all right here!</p>
+    </div>
+    <div class="row">
+        <div class = "column">
+            <div class = "card">
+                <img src="images/election_day.jpeg">
+                <div class ="container">
+                    <h2>2020 Election Meme</h2>
+                    <p>This meme was from the 2020 election between Joe Biden and Donald Trump.
+                        After a tense battle, this meme helped bring some joy back to the American people</p>
+                    </p>
+                    
+                </div>
+            </div>
+        </div>
+        <div class = "column">
+            <div class = "card">
+                <img src="images/evil_patrick.jpeg">
+                <div class ="container">
+                    <h2>Evil Patrick</h2>
+                    <p>A classic part of the Spongebob meme collection, this meme featuring Patrick Star stole the show for quite some time. Many memes of this nature were produced during that time and they remain very populary among the population.</p>
+                </div>
+            </div>
+        </div>
+        <div class = "column">
+            <div class = "card">
+                <img src="images/the_bern.jpeg">
+                <div class ="container">
+                    <h2>Sen. Sanders vs Darth Vader</h2>
+                    <p>During the 2021 inaguration ceremony, this picture was snapped of Senator Bernie Sanders and has since then been used to create some 10/10 memes.</p>
+                </div>
+            </div>
+        </div>
+        <div class = "column">
+            <div class = "card">
+                <img src="images/stonks.jpeg">
+                <div class ="container">
+                    <h2>Stonks</h2>
+                    <p>A true classic, the Stonks meme has been resurfacing each time something huge in the stock market happens. It is an oldi but surely still a goodie.</p>
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
